0db2b1a57568785528dc4f2cfcc622ac7f727759
Matt Haefling <m.haefling@protonmail.com>
2024-10-17 13:20:05 -0600

    feature: updated gem file and added registration_date method to vehicle class

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,7 +5,8 @@ class Vehicle
               :year,
               :make,
               :model,
-              :engine
+              :engine,
+              :registration_date
 
   def initialize(vehicle_details)
     @vin = vehicle_details[:vin]
@@ -21,5 +22,9 @@ class Vehicle
 
   def electric_vehicle?
     @engine == :ev
+  end
+
+  def registration_date
+    nil
   end
 end
