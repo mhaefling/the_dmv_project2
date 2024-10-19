8b5bf46dff6ba17a150aa165304c96a2c9abb511
Matt Haefling <m.haefling@protonmail.com>
2024-10-19 17:08:43 -0600

    feature: iteration3 - facility builder

--- lib/facility_builder.rb
+++ lib/facility_builder.rb
@@ -1,17 +1,45 @@
 class FacilityBuilder
+    attr_reader :facilities
+
     def initialize
-        @co_dmvs = []
-    def
+        @facilities = []
+
+        @co_dmv_data = DmvDataService.new.co_dmv_office_locations
+        @ny_dmv_data = DmvDataService.new.ny_dmv_office_locations
+        @mo_dmv_data = DmvDataService.new.mo_dmv_office_locations
+    end
 
     def create_facilities(data_set)
-        data_set.each do |dmv|
-        facility_details = {
-            name: dmv[:"dmv_office"],
-            address: "#{dmv[:"address_li"]} #{dmv[:"address_1"]} #{dmv[:"city"]} #{dmv[:"state"]} #{dmv[:"zip"]}",
-            phone: dmv[:"phone"]
-        }
-        @co_dmvs << Facility.new(facility_details)
-      end
-      require 'pry'; binding.pry
+
+        if data_set == @co_dmv_data
+            data_set.each do |dmv|
+                facility_details = {
+                    name: dmv[:"dmv_office"],
+                    address: "#{dmv[:"address_li"]} #{dmv[:"address_1"]} #{dmv[:"city"]} #{dmv[:"state"]} #{dmv[:"zip"]}",
+                    phone: dmv[:"phone"]
+                }
+                facilities << Facility.new(facility_details)
+            end
+
+        elsif data_set == @ny_dmv_data
+            data_set.each do |dmv|
+                facility_details = {
+                    name: dmv[:"office_name"],
+                    address: "#{dmv[:"street_address_line_1"]} #{dmv[:"city"]} #{dmv[:"state"]} #{dmv[:"zip_code"]}",
+                    phone: dmv[:"public_phone_number"]
+                }
+                facilities << Facility.new(facility_details)
+            end
+        
+        elsif data_set == @mo_dmv_data
+            data_set.each do |dmv|
+                facility_details = {
+                    name: dmv[:"name"],
+                    address: "#{dmv[:"address1"]} #{dmv[:"city"]} #{dmv[:"state"]} #{dmv[:"zipcode"]}",
+                    phone: dmv[:"phone"]
+                }
+                facilities << Facility.new(facility_details)
+            end
+        end 
     end
 end
